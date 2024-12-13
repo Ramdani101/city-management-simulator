832518d9a00887917ce85322072a5b411596e50e
Ramdani <dannymld41@gmail.com>
2024-12-13 23:38:28 +0700

    Menambahkan fungsi setColor(), printDengan2GarisBawah(), printTextTengah(), plusMinus(), memodifikasi tampilan menu utama dan fungsi campaignMode()

--- main.cpp
+++ main.cpp
@@ -1,21 +1,56 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <cmath>
 #include <ctime>
 #include <cstdint>
+#include <windows.h>
 using namespace std;
 
 /*
 Sertakan informasi tentang setiap variabel dan fungsi yang dipakai
 
 Varible :
-int pilihanMenuUtama : untuk menerima masukan pilihan di halaman utama
+int pilihan : untuk menerima masukan pilihan di halaman utama
+int lebarLayar : ini untuk lebar layar permainannya
+int jatahInteraksi : ini untuk jatah interaksi pemain
+int hari : untuk jatah hari;
 
 fungsi  :
 
 void campaignMode() : Fungsi untuk memulai game utama
+
 void howTo() : untuk menampilkan bagaimana sumber daya didalam game bekerja
+
 void keluar() : untuk memberhentikan program
+
+void menuUtama() : untuk menampilkan menu utama
+
+void printDengan2GarisBawah(string) : untuk print text dengan 2 garis bawah
+
+void setColor(int) : untuk ubah warna text, jadi ketika mau cout suatu teks dengan
+warna tertentu tinggal diset dulu, baru di cout. Terus ubah lagi ke putih.
+nah ini kode warnanya
+
+void printTengah(const string, int) : ini buat print text di tengah berdasarkan
+panjang yang kita masukkan diparameter int
+
+0 = Hitam
+1 = Biru Gelap
+2 = Hijau Gelap
+3 = Aqua Gelap
+4 = Merah Gelap
+5 = Ungu Gelap
+6 = Kuning Gelap
+7 = Putih (Default)
+8 = Abu-abu
+9 = Biru
+10 = Hijau
+11 = Aqua
+12 = Merah
+13 = Ungu
+14 = Kuning
+15 = Putih Cerah
 
 */
 
@@ -24,8 +59,17 @@ void campaignMode();
 void load();
 void howTo();
 int keluar();
+void setColor(int);
+void printDengan2GarisBawah(const string&);
+void printTextTengah(const string&, int);
+string plusMinus(int, int);
 
-int pilihanMenuUtama = 0;
+
+int pilihan = 0;
+int lebarLayar = 50;
+int jatahInteraksi = 3;
+int hari = 10;
+int uang = 0, populasi = 0, energi = 0, kebahagiaan = 0, pabrik = 0, rumah = 0, destinasiWisata = 0, sumberEnergi = 0;
 
 int main(){
     menuUtama();
@@ -33,12 +77,14 @@ int main(){
 }
 
 void menuUtama(){
-    cout << "Selamat Datang di CITY MANGAMENT SIMULATOR v1.0 \n"
-    << "--------------------------------- \n"
-    << "1. Start New Game \n" << "2. Load \n" << "3. How to \n" << "4. Exit \n";
+    setColor(6);
+    string selamatDatang = "Selamat Datang di CITY MANGAMENT SIMULATOR v1.0";
+    printDengan2GarisBawah(selamatDatang);
+    cout << "1. Start New Game \n" << "2. Load \n" << "3. How to \n" << "4. Exit \n";
+    setColor(7);
     cout << "Pilihan : ";
-    cin >> pilihanMenuUtama;
-    switch (pilihanMenuUtama)
+    cin >> pilihan;
+    switch (pilihan)
     {
     case 1 :
         campaignMode();
@@ -58,10 +104,47 @@ void menuUtama(){
 }
 
 void campaignMode(){
-    cout << "Sedang dalam tahap pengembangan \n";
+
+    uang = 10000;
+    energi = 30;
+    kebahagiaan = 70;
+    pabrik = 5;
+    rumah = 10;
+    populasi = rumah * 100;
+    destinasiWisata = 5;
+    sumberEnergi = 3;
+
+
+    cout << "Pada suatu pagi kamu tiba-tiba terbangun ditempat yang asing \n"
+    << "Tempat asing itu adalah kamar tidur walikota!! \n"
+    << "Kamu melihat kecermin dan menyadari dirimu sedang berada pada raga walikota \n"
+    <<  "Saat sedang kondisi terkejut, kemudian muncullah pesan dari ponselmu yang berisi \n"
+    << "-Walikota yang sebenarnya akan kembali dalam waktu 10 hari lagi- \n"
+    << "nasib kota ini berada di tanganmu sekarang !!! \n";
+    cout << endl;
+
+    cout << "Tekan apa saja untuk melanjutkan \n";
+    cin.ignore();
+    cin.get();
+
+    for (int i = 0; i < hari; i++)
+    {
+
+        setColor(6);
+        cout << string(lebarLayar, '-') << endl;
+        printTextTengah("Hari Ke - " + i+1, lebarLayar);
+        cout << string(lebarLayar, '-') << endl;
+
+        cout << "Uang : $" << uang << " | +$" << 3000*pabrik; << "/hari \n";
+        cout << "Populasi : " << populasi << endl;
+        cout << "Energi : " << 
+        
+    }
+    
+
     cout << "Tekan 1 untuk kembali \n";
-    cin >> pilihanMenuUtama;
-    if (pilihanMenuUtama == 1)
+    cin >> pilihan;
+    if (pilihan == 1)
     {
         menuUtama();
     }
@@ -72,8 +155,8 @@ void campaignMode(){
 void howTo(){
     cout << "Sedang dalam tahap pengembangan \n";
     cout << "Tekan 1 untuk kembali \n";
-    cin >> pilihanMenuUtama;
-    if (pilihanMenuUtama == 1)
+    cin >> pilihan;
+    if (pilihan == 1)
     {
         menuUtama();
     }
@@ -87,9 +170,36 @@ int keluar(){
 void load(){
     cout << "Sedang dalam tahap pengembangan \n";
     cout << "Tekan 1 untuk kembali \n";
-    cin >> pilihanMenuUtama;
-    if (pilihanMenuUtama == 1)
+    cin >> pilihan;
+    if (pilihan == 1)
     {
         menuUtama();
     }
+}
+
+void setColor(int color){
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+
+void printDengan2GarisBawah(const string& text){
+    cout << text << endl;
+    for (int i = 0; i < text.length(); i++)
+    {
+        cout << "=";
+    }
+    cout << endl;
+}
+
+void printTextTengah(const string& text, int lebar){
+    int padding  = (lebar - text.length())/2;
+    if (padding > 0)
+    {
+        cout << string(padding, ' ');
+    }
+    cout << text << endl
+}
+
+string plusMinus(int i, int j){
+    return (i > j)? "+" : "-";
 }
